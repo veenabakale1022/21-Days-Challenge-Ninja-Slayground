fa0f1f11182dc2db7bf025aec716b7c44f9284d7
veenabakale1022 <91620607+veenabakale1022@users.noreply.github.com>
2023-08-26 21:29:42 +0530

    Create Day14: Print Name.java

---
+++ Day14: Print Name.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+    static void printName(String s){
+        int i =0;
+        while(i<5){
+            System.out.println(s);
+            i++;
+        }
+        
+    }
+}
