9660c320d96c0498655234f37b1ec97c3f20b868
veenabakale1022 <91620607+veenabakale1022@users.noreply.github.com>
2023-08-28 09:29:44 +0530

    Create Day16:Largest Element in the Array.java

---
+++ Day16:Largest Element in the Array.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+
+        int largest = arr[0];
+
+        for(int i=1; i<n; i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        return largest;
+
+    }
+}
